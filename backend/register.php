1830730a5eef4df5e278d3a0b0c98d0ec243f609
Serhii Popov <popo0016@hz.nl>
2024-11-22 21:02:37 +0100

    fix bug with CORS

--- backend/register.php
+++ backend/register.php
@@ -5,6 +5,9 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     exit();
 }
 
