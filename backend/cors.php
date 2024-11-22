8b849a27f8ae0465122213295c309a7347313b8c
Serhii Popov <popo0016@hz.nl>
2024-11-22 21:15:59 +0100

    made a file for CORS

---
+++ backend/cors.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
